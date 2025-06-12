4d3ed41d149a6a049b740f5233e6d8f94d4e9bc6
Mbonleng Ernest <yungmonero29@gmail.com>
2025-06-12 13:35:18 +0100

    final commit

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-window.getProfile = function () {
-  const username = document.getElementById('username').value;
-  const profileDiv = document.getElementById('profile');
+function getProfile () {
+  const username = document.getElementById('username').value
+  const profileDiv = document.getElementById('profile')
 
   if (!username) {
-    profileDiv.innerHTML = '<p>Please enter a GitHub username.</p>';
-    return;
+    profileDiv.innerHTML = '<p>Please enter a GitHub username.</p>'
+    return
   }
 
   fetch(`https://api.github.com/users/${username}`)
     .then(response => {
       if (!response.ok) {
-        throw new Error('User not found');
+        throw new Error('User not found')
       }
-      return response.json();
+      return response.json()
     })
     .then(data => {
       profileDiv.innerHTML = `
@@ -35,10 +35,9 @@ window.getProfile = function () {
           </div>
           <a href="${data.html_url}" target="_blank">View GitHub Profile</a>
         </div>
-      `;
+      `
     })
     .catch(error => {
-      console.error(error);
-      profileDiv.innerHTML = '<p>User not found. Please try again.</p>';
-    });
-};
+      profileDiv.innerHTML = '<p>User not found. Please try again.</p>'
+    })
+}
